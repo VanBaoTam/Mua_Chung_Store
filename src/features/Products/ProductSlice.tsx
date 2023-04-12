9c6c4fb962b4a5f693cabdf1b1f1be5ca7fd67d2
Khanh Duong Ho <duonghk.tesse@gmail.com>
2023-04-12 21:00:13 +0700

    add accessToken

--- src/features/Products/ProductSlice.tsx
+++ src/features/Products/ProductSlice.tsx
@@ -1,7 +1,6 @@
 import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { ProductModel } from "../../models";
-import {} from "dotenv";
 const initArr: ProductModel[] = [];
 const initialState = {
   Products: initArr,
@@ -13,7 +12,7 @@ export const getProducts = createAsyncThunk(
   async (name, thunk) => {
     try {
       const resp = await axios.get(
-        "https://openapi.zalo.me/v2.0/mstore/product/getproductofoa",
+        "https://openapi.zalo.me/v2.0/mstore/product/getproductofoa?offset=0&limit=10",
         {
           params: {
             offset: "0",
@@ -21,7 +20,8 @@ export const getProducts = createAsyncThunk(
           },
           headers: {
             "Content-Type": "application/json",
-            access_token: "ACCESS_TOKEN",
+            access_token:
+              "e4LRDEqA9qwZG5qww0ydGgP3MJlf5tK7-5flBU5I5bJeQsnWz4PpVlXHQ7tLNrDfpszCL_vzVa76VdnOsri6PSrmVa-6IsbxjsjfKlbuLd-rBtq7ypDDUTyAC6hhA3Li_HepOV8K3MZ36GPTZ2mNV9q99WJRDZa3bYKZDTi093NZJY4yrKKS2SeE7YcCMG5LWMazLxjA67If06DwY2uQ1Rij1mRn4ZqCwXupDS0o94dT4HumpILc7_ebVHdeEsC1-0fwAkapSKlw0rzZrn5ZRkepSaF_F6aws2X9FFGZLp_ERr4On4yhRD1v6K74LI5ws7eaV_Su2s3pBHfkoMKP4dvwI4pg4I5B",
           },
         }
       );
