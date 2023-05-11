7b8bdbe9fcf3f80a34025db27b46ae6f38783dac
VanBaoTam <vanbaotam187@gmail.com>
2023-05-11 22:17:46 +0700

    "open GHTK"

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -269,9 +269,9 @@ const Cart = () => {
     let total = SumPrice(orders.Products);
     let ghtkProducts = ConvertCartProductModelsToGHTK(orders.Products);
     let uniqueGHTKVar = uniqueGHTK();
-    // if (paymentMethod == "COD")
-    //   await handleOrderOnGHTK(ghtkProducts, false, total, uniqueGHTKVar);
-    // else await handleOrderOnGHTK(ghtkProducts, true, total, uniqueGHTKVar);
+    if (paymentMethod == "COD")
+      await handleOrderOnGHTK(ghtkProducts, false, total, uniqueGHTKVar);
+    else await handleOrderOnGHTK(ghtkProducts, true, total, uniqueGHTKVar);
     setIsLoaded(true);
     setOrderSuccess(true);
   }
